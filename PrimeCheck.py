45b2db060972d2bb899e021082a878011a74fea2
Pavani-Choppa <116254743+Pavani-Choppa@users.noreply.github.com>
2026-07-19 22:40:15 +0530

    Create PrimeCheck.py

---
+++ PrimeCheck.py
@@ -0,0 +1,17 @@
+# Read the number
+num = int(input())
+
+if num <= 1:
+    print("Not Prime")
+else:
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime")
+    else:
+        print("Not prime")
