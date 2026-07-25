361f9f41919ab794aac112f7d652c4fc71d73ae5
Pavani-Choppa <116254743+Pavani-Choppa@users.noreply.github.com>
2026-07-25 15:56:05 +0530

    Update Sum of Squares.py

--- Sum of Squares.py
+++ Sum of Squares.py
@@ -2,9 +2,19 @@
 n = int(input())
 
 # Calculate and print sum of squares
-sumi = []
-sum = 0
-sumi = [x**2 for x in range(n+1)] 
-for i in sumi:
-    sum += i
-print("Sum:",sum)
+# sumi = []
+# sum = 0
+# sumi = [x**2 for x in range(n+1)] 
+# for i in sumi:
+#     sum += i
+# print("Sum:",sum)
+
+# Read N
+
+
+# Calculate and print sum of squares
+total = 0
+for i in range(n+1):
+    total += i*i
+print("Sum:",total)
+
